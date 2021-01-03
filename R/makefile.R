6fbf1aae2f59782a8edc004931d7832eb6e460af
Paul Julian <SwampThingPaul@users.noreply.github.com>
2021-01-02 21:11:51 -0500

    github action

---
+++ R/makefile.R
@@ -0,0 +1,7 @@
+# makefile
+
+# https://blog--simonpcouch.netlify.app/blog/r-github-actions-commit/
+# https://crontab.cronhub.io/
+# https://fromthebottomoftheheap.net/2020/04/30/rendering-your-readme-with-github-actions/
+
+usethis::use_github_actions()
